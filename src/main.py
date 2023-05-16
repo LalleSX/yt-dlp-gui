01aa9d564670f48391f06389167efa16af7205b7
Lalle <29478339+LalleSX@users.noreply.github.com>
2023-05-16 23:16:56 +0200

    Choose folder works better now

--- src/main.py
+++ src/main.py
@@ -4,7 +4,7 @@ import subprocess
 import threading
 import yt_dlp
 gi.require_version("Gtk", "4.0")
-from gi.repository import Gtk
+from gi.repository import Gtk, GLib
 
 class YoutubeDLApp(Gtk.Application):
     def __init__(self):
@@ -31,13 +31,22 @@ class YoutubeDLWindow(Gtk.ApplicationWindow):
         self.url_entry = Gtk.Entry()
         vbox.append(self.url_entry)
 
+        self.header = Gtk.HeaderBar()
+        self.set_titlebar(self.header)
+
         download_button = Gtk.Button(label="Download")
         download_button.connect("clicked", self.on_download_button_clicked)
         vbox.append(download_button)
 
+        folder_button = Gtk.Button(label="Choose Output Folder")
+        folder_button.connect("clicked", self.show_open_dialog)
+        vbox.append(folder_button)
+
         self.progress_label = Gtk.Label(label="")
         vbox.append(self.progress_label)
 
+        self.open_dialog = Gtk.FileDialog.new()
+        self.open_dialog.set_title("Select a Folder")
 
     def on_download_button_clicked(self, button):
         url = self.url_entry.get_text().strip()
@@ -47,22 +56,27 @@ class YoutubeDLWindow(Gtk.ApplicationWindow):
         self.progress_label.set_text("Downloading...")
         threading.Thread(target=self.download_video, args=(url,)).start()
         threading.Thread(target=self.download_thumbnail, args=(url,)).start()
-        
-    
+
     def download_video(self, url):
-        # If music.youtube.com is the URL then download the best audio only
-        if "music.youtube.com" in url:
-            yt_dlp.YoutubeDL({"format": "bestaudio"}).download([url])
-            success_message = "Download complete!"
-        else:
-            # Download the best video and audio
-            yt_dlp.YoutubeDL().download([url])
-            success_message = "Download complete!"
+        output_folder = self.open_dialog.get_file().get_path()
+        ydl_opts = {
+            "format": "bestaudio" if "music.youtube.com" in url else "best",
+            "outtmpl": os.path.join(output_folder, "%(title)s.%(ext)s"),
+        }
+        yt_dlp.YoutubeDL(ydl_opts).download([url])
+        success_message = "Download complete!"
         GLib.idle_add(self.progress_label.set_text, success_message)
 
     def download_thumbnail(self, url):
-        # Download thumbnail
-        yt_dlp.YoutubeDL({"writethumbnail": True}).download([url])
+        output_folder = self.open_dialog.get_file().get_path()
+        ydl_opts = {
+            "writethumbnail": True,
+            "outtmpl": os.path.join(output_folder, "%(title)s.%(ext)s"),
+        }
+        yt_dlp.YoutubeDL(ydl_opts).download([url])
+
+    def show_open_dialog(self, button):
+        self.open_dialog.open()
 
 if __name__ == "__main__":
     app = YoutubeDLApp()
